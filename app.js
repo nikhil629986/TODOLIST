a19277aee3dafb1167936f0a6512d3b76f690d3d
Nikhil Kumar <77457476+nikhil629986@users.noreply.github.com>
2022-07-06 00:38:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items=["Buy Food","Cook Food","Eat Food"];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+app.get("/", (req, res) => {
+
+  var today = new Date();
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long" 
+};
+var day=today.toLocaleDateString("en-US",options)
+
+  res.render("list",{kindofDay:day,newlistitems:items})
+});
+
+
+app.post("/", (req, res) => {
+  var item= req.body.newitem;
+  items.push(item);
+res.redirect("/")
+})
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
